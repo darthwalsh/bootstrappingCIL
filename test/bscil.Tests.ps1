6ecdb0d0d198548ea4202cb431f5130c11d65267
Carl Walsh <darthwalsh@gmail.com>
2014-05-21 00:20:36 -0700

    bscil2 testing
    copy over tests from bscil1 to ensure we don't lose existing
    functionality.

--- test/bscil.Tests.ps1
+++ test/bscil.Tests.ps1
@@ -87,6 +87,29 @@ Function TestBSCIL2($bscilexe) {
   It "runs echo2" {
     RunTest $bscilexe echo2.bscil2 "hello" "he"
   }
+  
+  It "runs blank" {
+    $exe = Compile $bscilexe blank.bscil2
+    
+    $output = & $exe
+    [string]($output).Length | Should be 0x52D0
+  }
+  
+  It "runs heart" {
+    RunTest $bscilexe heart.bscil2 "" "<3"
+  }
+  
+  It "runs echoTwice" {
+    RunTest $bscilexe echoTwice.bscil2 "abc" "cca"
+  }
+  
+  It "runs math" {
+    RunTest $bscilexe math.bscil2 "" "6"
+  }
+  
+  It "runs branches" {
+    RunTest $bscilexe branches.bscil2 "" "2456"
+  }
 }
 
 Remove-Item "*delete.exe" # Pretty safe to delete files ending in delete?
