a8d61d777039a4f7af51675d404d9ffbd2397a15
Carl Walsh <darthwalsh@gmail.com>
2021-11-23 18:40:09 -0800

    Test with Pester
    Instead of adding test filter, use a real test framework.
    The filter doesn't if the Add-Compilers aren't used, though.
    Rename Bin because that conficts with my global Get-Bin.

--- test/bscil.Tests.ps1
+++ test/bscil.Tests.ps1
@@ -1,71 +1,27 @@
 $ErrorActionPreference = "Stop"
 
-$sw = [system.diagnostics.stopwatch]::startNew()
+BeforeAll {
+  $sw = [system.diagnostics.stopwatch]::startNew()
+  $here = $PSScriptRoot
+  $root = Split-Path -Parent $here
 
-$here = Split-Path -Parent $MyInvocation.MyCommand.Path
-$root = Split-Path -Parent $here
-
-Function Root($p) {
-  Join-Path $root $p
-}
-
-Function Here($p) {
-  Join-Path $here $p
-}
-
-Function Bin($p) {
-  Here (Join-Path 'bin' $p)
-}
-
-Function Should-Be($expected) { 
-  Process { 
-    if ($_ -ne $expected) {
-      Get-PSCallStack | ForEach-Object {
-        Write-Warning (@($_.Command.PadLeft(16), ($_.Location -replace ' line ', '') , ($_.Arguments -replace '\s+', ' ')) -join ' ')
-      }
-      throw "Expected $expected but actually got $_"
-    }
+  Function Root($p) {
+    Join-Path $root $p
   }
-}
 
-$passed = 0
-$failed = 0
-
-Function It($name, $action) {
-  $time = Measure-Command { 
-    Try { 
-      & $action
-    } Catch { 
-      $e = $_
-    }
+  Function Here($p) {
+    Join-Path $here $p
   }
-  $time = [math]::Ceiling($time.TotalMilliseconds)
-  
 
-  if ($e) {
-    $color = "Red"
-    $success = "-"
-    ++$script:failed
+  Function BinDir($p) {
+    Here (Join-Path 'bin' $p)
   }
-  else {
-    $color = "DarkGreen"
-    $success = "+"
-    ++$script:passed
-  }
-  
-  Write-Host "[$success] $name $($time)ms" -Foreground $color
-  if ($e) {
-    Write-Host $e.ToString() -Foreground $color
-    throw ''
-  }
-}
 
-Function Describe($name, $action) {
-  Write-Host "Describing $name" -Foreground "magenta"
-  & $action
-}
+  Function Log($s) {
+    Write-Host "$($sw.ElapsedMilliseconds)ms $($s -replace (Root),'')" -Foreground DarkBlue
+  }
 
-Function CreateRuntimeConfig($name) {
+  Function CreateRuntimeConfig($name) {
     @{
       runtimeOptions = @{
         framework = @{
@@ -74,178 +30,222 @@ Function CreateRuntimeConfig($name) {
         }
       }
     } | ConvertTo-Json | Set-Content ($name -replace '\.exe$', '.runtimeconfig.json')
-}
-
-$compileI = 0
-Function Compile($exe, $target) {
-  # Don't overwrite existing files
-  $shortname = (Get-ChildItem $target).Name -replace '\W', '_'
-
-  $script:compileI++
-  $name = Bin "$($shortname)_$($script:compileI).exe"
-  $line = "$exe < $target > $name" # powershell destroys binary data in stream
-
-  if (Get-Command cmd -ErrorAction SilentlyContinue) {
-    cmd /c $line
   }
-  else {
-    CreateRuntimeConfig $exe
-    bash -c "dotnet $line"
-    CreateRuntimeConfig $name
-  }
-  
-  $LastExitCode | Should-Be 0
-  
-  $name
-}
 
-Function RunExe($exe, $instream = "") {
-  if (Get-Command cmd -ErrorAction SilentlyContinue) { 
-    $instream | & $exe 
-  }
-  else { 
-    $instream | & dotnet $exe 
-  }
-}
+  $compileI = 0
+  Function Compile($target) {
+    $target | Should -Not -BeNullOrEmpty
 
-Function RunTest($exe, $target, $instream, $expected) {
-  $target = Here $target
-  $exe = Compile $exe $target
+    # Don't overwrite existing files
+    $shortname = (Get-ChildItem $target).Name -replace '\W', '_'
+
+    $script:compileI++ # MAYBE use a stable name, then check file write time to reuse binary
+    $name = BinDir "$($shortname)_$($script:compileI).exe"
+    $compiler = Get-Compiler $target
+    $line = "$compiler < $target > $name" # MAYBE sc -raw (powershell destroys binary data in stream)
+
+    Log "exec compile $compiler $name"
+    if (Get-Command cmd -ErrorAction SilentlyContinue) {
+      cmd /c $line
+    }
+    else {
+      bash -c "dotnet $line"
+      CreateRuntimeConfig $name
+    }
     
-  $output = RunExe $exe $instream
-  if ($output -eq $null) {
-    $output = ""
-  }
+    $LastExitCode | Should -Be 0 -Because "Ran line $line"
     
-  [string]($output) | Should-Be $expected
+    $name
+  }
+
+  Function RunExe($exe, $instream = "") {
+    Log "exec $exe"
+    if (Get-Command cmd -ErrorAction SilentlyContinue) { 
+      $instream | & $exe 
+    }
+    else { 
+      $instream | & dotnet $exe 
+    }
+  }
+
+  Function RunTest($target, $instream, $expected) {
+    $target = Here $target
+    $exe = Compile $target
+      
+    $output = RunExe $exe $instream
+    if (-not $output) {
+      $output = ""
+    }
+      
+    [string]($output) | Should -Be $expected
+  }
+
+  Function Get-Description {
+    $____Pester.CurrentBlock.Name
+  }
+
+  $script:compilers = @{
+    bscil0 = Root "bscil0\bscil0.exe"
+  }
+
+  Function Get-Compiler($target = $null) {
+    $moniker = Get-Description
+    # MAYBE some logic like this needed so 
+    # $name, $ext = $moniker -split '\.'
+    # if ($target -and $name -ne ($target -split '\.')[-1]) {
+    #   Write-Warning "Unexpected earlier version!"
+
+    #   $prev = $name -replace '\d+', { -1 + [int]$_.Value }
+    #   $toolMoniker = "$ext.$name"
+    #   throw "TODO somehow make $prev   for mon $moniker tar $target "
+    # }
+
+    # if (-not $script:compilers.ContainsKey($moniker)) {
+    #   $source = Root (Join-Path $name $moniker)
+    #   Log "Creating $moniker using $source"
+    #   $script:compilers[$moniker] = Compile $source
+    # }
+
+
+    $script:compilers[$moniker]
+  }
+
+
+  if (Test-Path (BinDir)) {
+    Remove-Item (BinDir) -Recurse -Force
+  }
+  New-Item (BinDir) -ItemType Directory -Force
 }
 
-Function Bootstraps($bscilexe, $target) {
+Function Add-Compiler() {
+  It "compiles next tool" {
+    $name, $ext = (Get-Description) -split '\.'
+    $ext = $ext ?? $name
+    $next = if ($ext -eq $name) {
+      $name -replace '\d+', { 1 + [int]$_.Value }
+    }
+    else {
+      $name
+    }
+    $moniker = "$next.$name"
+    $source = Root "$next/$moniker"
+    Log "ADD $(Get-Description) Generating $moniker from $source"
+    $script:compilers[$moniker] = Compile $source
+  }
+}
+
+Function Bootstraps() {
   It "bootstraps" {
-    $bootstrapped = Compile $bscilexe $target
+    $version = (Get-Description) -split '\.' | Select-Object -First 1
+    $target = (Root "$version\$version.$version")
+    $bootstrapped = Compile $target
     
-    Get-Content $bscilexe -Raw | Should-Be (Get-Content $bootstrapped -Raw)
+    Get-Content (Get-Compiler) -Raw | Should -Be (Get-Content $bootstrapped -Raw)
   }
 }
 
-Function TestBSCIL0($bscilexe) {
+Function TestBSCIL0() {
   It "runs trivial" {
-    RunTest $bscilexe trivial.bscil0 "" ""
+    RunTest trivial.bscil0 "" ""
   }
   
   It "runs adder" {
-    RunTest $bscilexe adder.bscil0 "" "5"
+    RunTest adder.bscil0 "" "5"
   }
   
   It "runs echo2" {
-    RunTest $bscilexe echo2.bscil0 "hello" "he"
+    RunTest echo2.bscil0 "hello" "he"
   }
 }
 
-Function TestBSCIL1($bscilexe) {
+Function TestBSCIL1() {
   It "runs trivial" {
-    RunTest $bscilexe trivial.bscil1 "" ""
+    RunTest trivial.bscil1 "" ""
   }
   
   It "runs echo2" {
-    RunTest $bscilexe echo2.bscil1 "hello" "he"
+    RunTest echo2.bscil1 "hello" "he"
   }
   
   It "runs blank" {
-    $exe = Compile $bscilexe (Here blank.bscil1)
+    $exe = Compile (Here blank.bscil1)
     
     $output = RunExe $exe
-    [string]($output).Length | Should-Be 0x52CD
+    [string]($output).Length | Should -Be 0x52CD
   }
   
   It "runs heart" {
-    RunTest $bscilexe heart.bscil1 "" "<3"
+    RunTest heart.bscil1 "" "<3"
   }
   
   It "runs echoTwice" {
-    RunTest $bscilexe echoTwice.bscil1 "abc" "cca"
+    RunTest echoTwice.bscil1 "abc" "cca"
   }
   
   It "runs math" {
-    RunTest $bscilexe math.bscil1 "" "6"
+    RunTest math.bscil1 "" "6"
   }
   
   It "runs branches" {
-    RunTest $bscilexe branches.bscil1 "" "2456"
+    RunTest branches.bscil1 "" "2456"
   }
 }
 
-Function TestBSCIL2($bscilexe) {
+Function TestBSCIL2() {
   It "runs trivial" {
-    RunTest $bscilexe trivial.bscil2 "" ""
+    RunTest trivial.bscil2 "" ""
   }
   
   It "runs echo2" {
-    RunTest $bscilexe echo2.bscil2 "hello" "he"
+    RunTest echo2.bscil2 "hello" "he"
   }
   
   It "runs blank" {
-    $exe = Compile $bscilexe (Here blank.bscil2)
+    $exe = Compile (Here blank.bscil2)
     
     $output = RunExe $exe
-    [string]($output).Length | Should-Be 0x52CD
+    [string]($output).Length | Should -Be 0x52CD
   }
   
   It "runs heart" {
-    RunTest $bscilexe heart.bscil2 "" "<3"
+    RunTest heart.bscil2 "" "<3"
   }
   
   It "runs echoTwice" {
-    RunTest $bscilexe echoTwice.bscil2 "abc" "cca"
+    RunTest echoTwice.bscil2 "abc" "cca"
   }
   
   It "runs math" {
-    RunTest $bscilexe math.bscil2 "" "6"
+    RunTest math.bscil2 "" "6"
   }
   
   It "runs branches" {
-    RunTest $bscilexe branches.bscil2 "" "2456"
+    RunTest branches.bscil2 "" "2456"
   }
   
   It "runs mem" {
-    RunTest $bscilexe mem.bscil2 "4" "34"
+    RunTest mem.bscil2 "4" "34"
   }
 }
 
-if (Test-Path (Bin)) {
-  Remove-Item (Bin) -Recurse -Force
-}
-New-Item (Bin) -ItemType Directory -Force
-
-$bscil0exe = Root "bscil0\bscil0.exe"
 
 Describe "bscil0" {
-  TestBSCIL0 $bscil0exe
-  
-  Bootstraps $bscil0exe (Root "bscil0\bscil0.bscil0")
+  TestBSCIL0
+  Bootstraps
+  Add-Compiler
 }
 
-$bscil1exe = Compile $bscil0exe (Root "bscil1\bscil1.bscil0")
-
 Describe "bscil1.bscil0" {
-  TestBSCIL1 $bscil1exe 
+  TestBSCIL1
+  Add-Compiler
 }
 
 Describe "bscil1.bscil1" {
-  $anotherbscil1 = Compile $bscil1exe (Root "bscil1\bscil1.bscil1")
-  TestBSCIL1 $anotherbscil1
-  
-  Bootstraps $anotherbscil1 (Root "bscil1\bscil1.bscil1")
+  TestBSCIL1
+  Bootstraps
+  Add-Compiler
 }
-
-$bscil2exe = Compile $bscil1exe (Root "bscil2\bscil2.bscil1")
 
 Describe "bscil2.bscil1" {
-  TestBSCIL2 $bscil2exe 
+  TestBSCIL2
 }
-
-$time = $sw.ElapsedMilliseconds
-Write-Host "Tests completed in $($time)ms"
-Write-Host "Passed: $passed Failed: $failed"
-
-exit $failed
